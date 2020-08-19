559c3b51aaed22857bc860231e878033a48905cb
xuantinfx <xuantinfx@gmail.com>
2020-08-19 20:13:44 +0700

    Update config for deploy

---
+++ config/dbconfig.sample.js
@@ -0,0 +1,3 @@
+export default {
+  connectionString: process.env.MONGODB_CONNECTION_STRING,
+}
--- config/dbconfig.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  connectionString: process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017/sync-google-fit',
-}

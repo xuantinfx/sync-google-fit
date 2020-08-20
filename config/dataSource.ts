f94e7e3014cca2a2be91c9f573176dd74990cc7c
xuantinfx <xuantinfx@gmail.com>
2020-08-20 12:40:51 +0700

    Updated datasource for amafit and mifit on iphone

--- config/dataSource.ts
+++ config/dataSource.ts
@@ -1,4 +1,6 @@
 export default [
   'raw:com.google.step_count.delta:com.fossil.wearables.fossil:Fossil Hybrid:UNKNOWN:D0F00119RW:',
   'raw:com.google.step_count.delta:com.xiaomi.hm.health:',
+  'raw:com.google.step_count.delta:com.huami.watch.hmwatchmanager:',
+  'derived:com.google.step_count.delta:com.google.fitkit:apple:iphone:hm.wristband:local_data',
 ]
